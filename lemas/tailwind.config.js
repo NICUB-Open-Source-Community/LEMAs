99d7778e23661775eb2904c95c9768478fac285d
nateos <sodjanathan@gmail.com>
2024-08-03 22:47:21 +0000

    config: prettier, react-icons, flowbite

--- lemas/tailwind.config.js
+++ lemas/tailwind.config.js
@@ -1,10 +1,14 @@
+const flowbite = require("flowbite-react/tailwind");
+
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}", flowbite.content()],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Inter", "system-ui", "Arial", "sans-serif"],
+        serif: ["Merriweather", "Georgia", "serif"],
+      },
+    },
   },
   plugins: [require("flowbite/plugin")],
 };
