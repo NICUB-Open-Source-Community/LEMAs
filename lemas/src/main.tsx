75a463d2ee21bb333c2f87a329a88085d884e252
nateos <sodjanathan@gmail.com>
2024-08-03 22:48:39 +0000

    feat: signup, building page

--- lemas/src/main.tsx
+++ lemas/src/main.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App.tsx";
-import SignUp from "./routes/SignUp.tsx"
-import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import SignUp from './routes/SignUp.tsx'
+import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 
 const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <div>Hello world!</div>,
-  },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/app",
-    element: <App />,
-  },
-]);
+    {
+        path: '/',
+        element: <div>Hello world!</div>,
+    },
+    {
+        path: '/signup',
+        element: <SignUp />,
+    },
+    {
+        path: '/app',
+        element: <App />,
+    },
+])
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <RouterProvider router={router} />
-  </React.StrictMode>,
-);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+)
--- lemas/src/routes/SignUp.tsx
+++ lemas/src/routes/SignUp.tsx
@@ -1,7 +1,65 @@
-import React from "react";
+'use client'
 
-function SignUp() {
-  return <div className="py-6 text-xs">SignUp</div>;
+import { Label, TextInput, Button } from 'flowbite-react'
+import { IoMdContact, IoIosMail } from 'react-icons/io'
+import { FaPhoneAlt } from 'react-icons/fa'
+import { MdOutlineLocationOn } from 'react-icons/md'
+import { IoMdEye } from 'react-icons/io'
+
+export function SignUp() {
+  return (
+    <div className="">
+      <div className="signup-container">
+        <h1 className="font-sans text-5xl my-14">LEMAs App</h1>
+        <form action="">
+          <div className="max-w-md">
+            <TextInput
+              className=""
+              id="fullname"
+              type="fullname"
+              rightIcon={IoMdContact}
+              placeholder="Full Name"
+              required
+            />
+            <TextInput
+              className="mt-1"
+              id="email"
+              type="email"
+              rightIcon={IoIosMail}
+              placeholder="Email Address"
+              required
+            />
+            <TextInput className="mt-1" id="phone" type="phone" rightIcon={FaPhoneAlt} placeholder="Phone" required />
+            <TextInput
+              className="mt-1"
+              id="city"
+              type="city"
+              rightIcon={MdOutlineLocationOn}
+              placeholder="City"
+              required
+            />
+            <TextInput className="mt-1" id="city" type="city" rightIcon={IoMdEye} placeholder="Password" required />
+            <TextInput
+              className="mt-1"
+              id="city"
+              type="city"
+              rightIcon={MdOutlineLocationOn}
+              placeholder="Confirm Password"
+              required
+            />
+          </div>
+          <Button className="my-1" type="submit">
+            Register
+          </Button>
+          <div className="mt-6">
+            <p className='text-base'>
+              Already have an account? <a href="">Sign In</a>
+            </p>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
 }
 
-export default SignUp;
+export default SignUp
